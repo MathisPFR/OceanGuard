f6dbaede2ab220755962a0691f4488ade110762a
MathisPFR <mathispetitfr@gmail.com>
2024-12-05 14:33:24 +0100

    feat(zmp): récupérer et afficher les zones marines protégées

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ZmpComponent } from "./components/zmp/zmp.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ZmpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
