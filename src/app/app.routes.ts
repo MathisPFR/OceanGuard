105a935222bc6170e0fdee2cb99fde0ffde51cf1
MathisPFR <139142700+MathisPFR@users.noreply.github.com>
2024-12-06 16:35:41 +0100

    Revert "Feat/zmp map"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { ZmpComponent } from './components/zmp/zmp.component';
 import { PostZmpComponent } from './components/post-zmp/post-zmp.component';
 import { UpdateZmpComponent } from './components/update-zmp/update-zmp.component';
-import { MapComponent } from './components/map/map.component';
 
 
 export const routes: Routes = [
@@ -11,5 +10,4 @@ export const routes: Routes = [
   { path: 'post-zmp', component: PostZmpComponent }, 
   { path: 'post-zmp', component: PostZmpComponent },
   { path: 'update-zmp/:id', component: UpdateZmpComponent }, 
-  { path: 'map', component: MapComponent }, 
 ];
